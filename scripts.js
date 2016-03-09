8db21b3f046fe1a733f40a2f73eb4297f4bedac2
Tom Moore <tom5265@gmail.com>
2016-03-09 15:47:36 -0600

    functions working with buttons

--- scripts.js
+++ scripts.js
@@ -9,3 +9,39 @@ function divider() {
         }
     });
 }
+
+function adder() {
+    var num1 = document.getElementById('one').value;
+    var num2 = document.getElementById('two').value;
+    davidsLib.add(num1, num2, function(err, result) {
+        if (err) {
+            alert('There was a problem, bro');
+        } else {
+            alert(result);
+        }
+    });
+}
+
+function multiplier() {
+    var num1 = document.getElementById('one').value;
+    var num2 = document.getElementById('two').value;
+    davidsLib.multiply(num1, num2, function(err, result) {
+        if (err) {
+            alert('There was a problem, bro');
+        } else {
+            alert(result);
+        }
+    });
+}
+
+function subtracting() {
+    var num1 = document.getElementById('one').value;
+    var num2 = document.getElementById('two').value;
+    davidsLib.subtract(num1, num2, function(err, result) {
+        if (err) {
+            alert('There was a problem, bro');
+        } else {
+            alert(result);
+        }
+    });
+}
