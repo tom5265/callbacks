fd6323093123c1bc7d4e9c35c61303f1b6091006
Tom Moore <tom5265@gmail.com>
2016-03-09 16:23:33 -0600

    added flashing loading extra credit

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 function divider() {
     var num1 = document.getElementById('one').value;
     var num2 = document.getElementById('two').value;
+    var para = document.createElement('p');
+    para.innerHTML = 'loading...';
+    var running = document.getElementById('running');
+    running.appendChild(para);
     davidsLib.divide(num1, num2, function(err, result) {
         if (err) {
             alert('There was a problem, bro');
+            running.removeChild(para);
         } else {
             alert(result);
+            running.removeChild(para);
         }
     });
 }
@@ -13,11 +19,17 @@ function divider() {
 function adder() {
     var num1 = document.getElementById('one').value;
     var num2 = document.getElementById('two').value;
+    var para = document.createElement('p');
+    para.innerHTML = 'loading...';
+    var running = document.getElementById('running');
+    running.appendChild(para);
     davidsLib.add(num1, num2, function(err, result) {
         if (err) {
             alert('There was a problem, bro');
+            running.removeChild(para);
         } else {
             alert(result);
+            running.removeChild(para);
         }
     });
 }
@@ -25,11 +37,17 @@ function adder() {
 function multiplier() {
     var num1 = document.getElementById('one').value;
     var num2 = document.getElementById('two').value;
+    var para = document.createElement('p');
+    para.innerHTML = 'loading...';
+    var running = document.getElementById('running');
+    running.appendChild(para);
     davidsLib.multiply(num1, num2, function(err, result) {
         if (err) {
             alert('There was a problem, bro');
+            running.removeChild(para);
         } else {
             alert(result);
+            running.removeChild(para);
         }
     });
 }
@@ -37,11 +55,17 @@ function multiplier() {
 function subtracting() {
     var num1 = document.getElementById('one').value;
     var num2 = document.getElementById('two').value;
+    var para = document.createElement('p');
+    para.innerHTML = 'loading...';
+    var running = document.getElementById('running');
+    running.appendChild(para);
     davidsLib.subtract(num1, num2, function(err, result) {
         if (err) {
             alert('There was a problem, bro');
+            running.removeChild(para);
         } else {
             alert(result);
+            running.removeChild(para);
         }
     });
 }
